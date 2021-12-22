6d03857159e12cbab8fae73b4c91dd0724f8b97a
Nelite <neliitsme14@gmail.com>
2021-12-22 13:26:22 +0300

    fix: fix footer on pages other than index

--- global-scripts/load-time.js
+++ global-scripts/load-time.js
@@ -4,7 +4,7 @@ window.onload = getLoadTime;
 function getLoadTime() {
     var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
     var footerTime = document.querySelector(".footer__load-time");
-    footerTime.innerHTML = `Site loaded in ${loadTime / 1000}s`;
+    footerTime.innerHTML = `Page loaded in ${loadTime / 1000}s`;
     // console.log(loadTime/1000);
     // console.log((new Date().getTime() - start) / 1000);
 }
